215239c958c6ccec4d8dfd0527710fb654e06f75
Summerlight <101summerlight@gmail.com>
2017-10-30 10:59:13 +0800

    [Phase two: Perfection] Going database.

--- pm25forecast/views.py
+++ pm25forecast/views.py
@@ -1,12 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def forecast(request, station_id):
-    context = {'station_id': station_id}
+    '''
+    import mysql.connector
+    cnx = mysql.connector.connect(user='root',password='360chenghua', host='127.0.0.1',database='pm25_readings')
+    cursor = cnx.cursor()
+    timestrings = "('20171030', '1'),('20171030', '4')"
+    query = "select ID, DATE, HOUR, READING from readings where ID = %s and (DATE, HOUR) IN (" + timestrings + ")"
+    cursor.execute(query,(station_id,))
+
+    data = []
+    for (id, date, hour, reading) in cursor:
+        record = {"ID": id, "DATE": date, "HOUR": hour, "READING": reading}
+        data.append(record)
+
+    cursor.close()
+    cnx.close()
+
+    import pandas as pd
+    queryResult = pd.DataFrame(data)
+    queryResult['TIMESTAMP'] = queryResult['DATE'] + queryResult['HOUR']
+    final = queryResult.drop(["DATE", "HOUR"], axis=1)
+    final.to_csv(os.path.join(BASE_DIR, "static/" + station_id + ".csv"), index=False)
+    '''
+    context = {'station_id': station_id, 'filename': (station_id + ".csv")}
     return render(request, 'forecast-page.html', context)
 
 def experiment(request):
