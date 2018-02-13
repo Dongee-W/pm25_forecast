8b600687660fa123d4baa412383a8b1181807da9
Summerlight <101summerlight@gmail.com>
2018-02-13 21:11:40 +0800

    Herro blizard

--- pm25forecast/views.py
+++ pm25forecast/views.py
@@ -530,6 +530,7 @@ def epamain(request):
     outOfDateString = (str(outOfDate.year) + '{0:02d}'.format(outOfDate.month) + '{0:02d}'.format(outOfDate.day),)
 
     cursor.execute(query, outOfDateString)
+    name=current.strftime('%Y%m%d%H')
 
     data = []
     for (model_id, hour_ahead, prediction, real) in cursor:
